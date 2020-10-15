68bc649d2c6bd36300754d8cd001d939069d4104
NayoungLee240 <iny003@naver.com>
2020-10-15 17:45:13 +0900

    Add MainActivty, 좌측 메뉴 기본 구성

--- app/src/main/java/com/nyoungee/memohaseo/MainActivity.java
+++ app/src/main/java/com/nyoungee/memohaseo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.nyoungee.memohaseo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Main Title Bar Setting
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //titlebar_main.xml로 셋팅
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.titlebar_main, null);
+
+        actionBar.setCustomView(actionbar);
+
+        Toolbar parent = (Toolbar) actionbar.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+
+        return true;
     }
 }
